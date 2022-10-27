5f41a5f5d4864e1741464f6ba22f41e797337dd7
edgar_lopez <edgarlopezacevedo@gmail.com>
2022-10-27 13:32:16 -0500

    beta Portafolio

---
+++ js/index.js
@@ -0,0 +1,36 @@
+window.sr=ScrollReveal();
+
+
+sr.reveal('.inicio', {
+    duration:3000,
+    origin:'bottom',
+    distance:'-100px'
+
+});
+sr.reveal('.estudios', {
+    duration:3000,
+    origin:'top',
+    distance:'-100px'
+
+});
+
+sr.reveal('.sobremi', {
+    duration:3000,
+    origin:'bottom',
+    distance:'-100px'
+
+});
+
+sr.reveal('.skills', {
+    duration:3000,
+    origin:'left',
+    distance:'-100px'
+
+});
+
+sr.reveal('.experiencia', {
+    duration:3000,
+    origin:'bottom',
+    distance:'-100px'
+
+});
